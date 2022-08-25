3ec8201cb98cfaf17f275550e206469cad0bb2ff
HolidayLOL <102799984+HolidayLOL@users.noreply.github.com>
2022-08-25 12:12:45 +0300

    Refactoring class Main (#1)

--- src/Holiday/Main.java
+++ src/Holiday/Main.java
@@ -6,19 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
-        while ( true ) {
-            System.out.print("Type here > ");
-            String s = scanner.nextLine();
-            int uppers = 0, lowers = 0;
-
-            for ( char c : s.toCharArray() ) {
-                if ( Character.isUpperCase(c) )
-                    ++uppers;
-                else if ( Character.isLowerCase(c) )
-                    ++lowers;
-            }
-            System.out.println("UpperCase: " + uppers + "\nLowerCase: " + lowers );
+        System.out.println(LettersNumberCalculator.calculate(scanner));
         }
     }
-}
